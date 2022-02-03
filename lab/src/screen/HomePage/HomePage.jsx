44aee4d6559e1f90af29b799cc97b888ce7d0e70
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-02-03 12:26:07 -0500

    HomePage and Test merged up

--- lab/src/screen/HomePage/HomePage.jsx
+++ lab/src/screen/HomePage/HomePage.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export default function HomePage() {
+export default function HomePage(props) {
  
   return <div>
     <h1>Hello BootCamp Grad!</h1>
+    <div>{props.children}</div>
       </div>
 }
