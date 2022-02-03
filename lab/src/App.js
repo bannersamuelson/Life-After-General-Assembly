0c361bc86a752eb252ffc1537109f7494a9208d2
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-02-02 21:19:42 -0500

    add Course

--- lab/src/App.js
+++ lab/src/App.js
@@ -5,6 +5,7 @@ import SignUp from "./screen/SignUp/SignUp.jsx"
 import Login from "./screen/Login/Login.jsx"
 import Layout from './components/Layout/Layout';
 import { useState } from 'react';
+import AddCourse from './components/AddCourse/AddCourse';
 function App() {
   const [user, setUser] = useState();
   // const [course, setCourse] = useState();
@@ -15,7 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout user={user} setUser={setUser}><Test user={user} setUser={setUser}/></Layout>}/>
         <Route path="/signup" element={<Layout user={user} setUser={setUser}><SignUp user={user} setUser={setUser}/></Layout>} />
-        <Route path="/login" element={<Layout user={user} setUser={setUser}><Login user={user} setUser={setUser}/></Layout>} />
+        <Route path="/login" element={<Layout user={user} setUser={setUser}><Login user={user} setUser={setUser} /></Layout>} />
+        <Route path="/addcourse" element={<Layout user={user} setUser={setUser}><AddCourse user={user} setUser={setUser}/></Layout>} />
       </Routes>
     </div>
   );
--- lab/src/components/AddCourse/AddCourse.jsx
+++ lab/src/components/AddCourse/AddCourse.jsx
@@ -1,7 +1,123 @@
-import React from 'react';
+import React, {  useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { createUser } from "../../services/apiconfig"; 
 
 export default function AddCourse() {
-  return <div>
-    
-  </div>;
+  const [newUser, setNewUser] = useState({
+    user: "",
+    title: "",
+    image: "",
+    content: "",
+    link: "",
+    review: "",
+    rate: 0,
+  });
+  // const [validationMessage, setValidationMessage] = useState("");
+  // const [valid, setValid] = useState(false);
+  const navigation = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await createUser(newUser);
+    console.log(newUser);
+    navigation("/login");
+  };
+
+  const handleInput = (e) => {
+    const { id, value } = e.target;
+    setNewUser((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  // useEffect(() => {
+  //   console.log("validator");
+  //   const res = signUp(newUser);
+  //   console.log(res);
+  //   checkIfValid();
+  // }, [newUser.password, newUser.confirmPassword]);
+
+
+  return (<div >
+  <form onSubmit={handleSubmit}>
+    <h2 >Add a Course</h2>
+    {/* <h3 >{validationMessage}</h3> */}
+    <label >User</label>
+    <br />
+    <input
+      type="text"
+      placeholder="create user name"
+      id="user"
+      value={newUser.user}
+      onChange={handleInput}
+    />
+    <br />
+    <label >Upload Title</label>
+    <br />
+    <input
+      type="text"
+      placeholder="title"
+      id="title"
+      value={newUser.title}
+      onChange={handleInput}
+    />
+    <br />
+    <label >Image link</label>
+    <br />
+    <input
+      type="text"
+      placeholder="image"
+      id="image"
+      value={newUser.image}
+      onChange={handleInput}
+    />
+    <br />
+    <label>Content</label>
+    <br />
+    <input
+      type="text"
+      placeholder="content"
+      id="content"
+      value={newUser.content}
+      onChange={handleInput}
+    />
+    <br />
+    <label>link</label>
+    <br />
+    <input
+      type="text"
+      placeholder="link"
+      id="link"
+      value={newUser.link}
+      onChange={handleInput}
+    />
+    <br />
+    <label>Review</label>
+    <br />
+    <input
+      type="text"
+      placeholder="review"
+      id="review"
+      value={newUser.review}
+      onChange={handleInput}
+    />
+    <br />
+    <label>rate</label>
+    <br />
+    <input
+      type="number"
+      placeholder="rate"
+      id="rate"
+      value={newUser.rate}
+      onChange={handleInput}
+    />
+    <br />
+    <br />
+    <button>
+      SIGN UP
+    </button>
+  </form>
+</div>
+);
 }
