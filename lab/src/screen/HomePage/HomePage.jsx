ed03acaefa45a8d2c71a6dd16b8430173896f984
Banner Samuelson <bannersamuelson@gmail.com>
2022-02-03 18:53:08 -0500

    gitignore

--- lab/src/screen/HomePage/HomePage.jsx
+++ lab/src/screen/HomePage/HomePage.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 export default function HomePage(props) {
- 
+
   return (
-      <body className="bg-gradient-to-r from-blue-800 to-blue-900 text-gray-50">
+    <body className="bg-gradient-to-r from-blue-800 to-blue-900 text-gray-50">
       <div id="app">
-       <div className="flex items-center h-16 px-6 border-b border-gray-700">
-              <div className="flex items-center ml-auto">Banner</div>
-         </div> 
-        
+        <div className="flex items-center h-16 px-6 border-b border-gray-700">
+          <div className="flex items-center ml-auto">Banner</div>
+        </div>
+
         <div className="w-full border-b border-gray-700">
           <div className="flex flex-wrap items-center max-w-screen-xl px-10 py-20 mx-auto md:flex-nowrap">
             <div className="flex flex-col items-start justify-center flex-grow w-full pr-0 md:w-0 md:pr-10">
-            <span className="w-full font-bold text-center text-gray-300 md:text-left">FULL STACK</span>
-            <h1 className="w-full mt-4 text-5xl font-black text-center lg:text-6xl md:text-left">LIFE AFTER GA</h1>
-            <p className="w-full mt-6 text-center text-gray-300 md:text-left">This website is dedicated to bettering the life of coders by providing course materials and realtime reviews for the everyday applications.</p>
-          <div className="flex flex-col items-center justify-center w-full mt-8 md:flex-row md:justify-start"></div>
-          <Link to="/front" className="flex items-center h-12 px-6 text-sm bg-blue-600 rounded-lg text-blue-50 hover:bg-blue-700">Go to courses!</Link>
+              <span className="w-full font-bold text-center text-gray-300 md:text-left">FULL STACK</span>
+              <h1 className="w-full mt-4 text-5xl font-black text-center lg:text-6xl md:text-left">LIFE AFTER GA</h1>
+              <p className="w-full mt-6 text-center text-gray-300 md:text-left">This website is dedicated to bettering the life of coders by providing course materials and realtime reviews for the everyday applications.</p>
+              <div className="flex flex-col items-center justify-center w-full mt-8 md:flex-row md:justify-start"></div>
+              <Link to="/front" className="flex items-center h-12 px-6 text-sm bg-blue-600 rounded-lg text-blue-50 hover:bg-blue-700">Go to courses!</Link>
               <Link to="/signup" className="mt-4 text-sm text-gray-400 underline md:mt-0 md:ml-4 hover:text-gray-200">join the club</Link>
-          <img className="flex-grow w-full md:w-0" src="https://insights.dice.com/wp-content/uploads/2019/07/Software-Developer-Software-Engineer-Dice.png" alt="computerguy"/>
-          
-          
+              <img className="flex-grow w-full" src="https://insights.dice.com/wp-content/uploads/2019/07/Software-Developer-Software-Engineer-Dice.png" alt="computerguy" />
+
+
             </div>
           </div>
         </div>
 
 
 
-        </div>
-      </body>
+      </div>
+    </body>
 
 
 
