c153b4e665cb3a669c0bb85fbeb606ae134b86df
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-02-03 12:36:17 -0500

    hompage and test combined style in tailwinds css

--- lab/src/screen/HomePage/HomePage.jsx
+++ lab/src/screen/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function HomePage(props) {
  
   return <div>
-    <h1>Hello BootCamp Grad!</h1>
-    <div>{props.children}</div>
+    <h1 style={{textAlign:"center",fontFamily:"Nunito"}}>Hello BootCamp Grad!</h1>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">{props.children}</div>
       </div>
 }
