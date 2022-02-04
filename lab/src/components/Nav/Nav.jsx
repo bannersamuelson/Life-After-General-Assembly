d0a40bf595e2c1817feeca1b94707b018b7bc11f
Banner Samuelson <bannersamuelson@gmail.com>
2022-02-04 13:54:02 -0500

    updated

--- lab/src/components/Nav/Nav.jsx
+++ lab/src/components/Nav/Nav.jsx
@@ -30,7 +30,7 @@ const alwaysOptions = (
 const Nav = (props) => {
 
   return (
-    <nav className="bg-amber-400  text-gray-50 border-gray-800 px-2 sm:px-4 py-2.5 dark:bg-gray-800 
+    <nav className="bg-gray-800  text-gray-50 border-gray-800 px-2 sm:px-4 py-2.5
     flex items-center h-16 border-b">
 
       <div className="container flex flex-wrap justify-between items-center mx-auto">
@@ -41,7 +41,7 @@ const Nav = (props) => {
         <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1">
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             {props.user && props.loginUser && <div className="link welcome">Welcome, {props.loginUser}</div>}
-                 {alwaysOptions}
+            {alwaysOptions}
             {props.user && props.loginUser ? authenticatedOptions : unauthenticatedOptions}
           </ul>
         </div>
