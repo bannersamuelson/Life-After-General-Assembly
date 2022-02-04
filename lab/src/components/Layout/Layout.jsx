29cc17af3f7928b49831f69bf11f56972f4df055
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-02-03 20:16:20 -0500

    updated layout

--- lab/src/components/Layout/Layout.jsx
+++ lab/src/components/Layout/Layout.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import Nav from '../Nav/Nav';
 
 export default function Layout(props) {
-  const handleLogout = (e) => {
-    e.preventDefault();
-    localStorage.removeItem("token");
-    window.location.reload(false);
-    console.log("removed");
-  }
-  return <div>
-    <nav>
-      <a href="/" style={{textDecoration:"none"}}> Home </a>
-      <a href="/signup" style={{textDecoration:"none"}}> Signup </a>
-      <a href="/login" style={{ textDecoration: "none" }}> Login </a>
-      <a href="/login" style={{ textDecoration: "none" }} onClick={(e) => { handleLogout(e) }}> Logout </a>
-      <a href="/addcourse" style={{textDecoration:"none"}}> Add a Course</a>
-      </nav>
-    <div>{props.children}</div>
-  </div>;
+
+  return (
+    <div className='layout'>
+      <Nav user={props.user} />
+      <div className="layout-children">
+          {props.children}
+      </div>
+    </div>
+
+  //   <div>
+  //   <nav>
+  //     <a href="/" style={{textDecoration:"none"}}> Home </a>
+  //     <a href="/signup" style={{textDecoration:"none"}}> Signup </a>
+  //     <a href="/login" style={{ textDecoration: "none" }}> Login </a>
+  //     <a href="/login" style={{ textDecoration: "none" }} onClick={(e) => { handleLogout(e) }}> Logout </a>
+  //     <a href="/addcourse" style={{textDecoration:"none"}}> Add a Course</a>
+  //     </nav>
+  //   <div>{props.children}</div>
+  // </div> 
+)
 }
