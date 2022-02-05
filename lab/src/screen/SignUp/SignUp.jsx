ec0d67ad7746e67d0ef0b832cf643fe968b1b822
Banner Samuelson <bannersamuelson@gmail.com>
2022-02-04 22:03:04 -0500

    fixed sizing

--- lab/src/screen/SignUp/SignUp.jsx
+++ lab/src/screen/SignUp/SignUp.jsx
@@ -60,7 +60,7 @@ export default function SignUp(props) {
     <div className="grid justify-items-center mt-10">
       <form onSubmit={handleSubmit} class="w-full max-w-lg">
         <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <div class="w-full px-3 mb-6">
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               Username
             </label>
@@ -71,9 +71,9 @@ export default function SignUp(props) {
               name="username"
               id="userName"
               placeholder="coreygibson"
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" />
+              className="focus:placeholder-transparent appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" />
           </div>
-          <div class="w-full mb-6 md:w-1/2 px-3">
+          <div class="w-full mb-6 md:w-1/2 md:mb-0 px-3">
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               First Name
             </label>
@@ -84,9 +84,9 @@ export default function SignUp(props) {
               name="floating_first_name"
               id="firstName"
               placeholder="john"
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-200" />
+              className="focus:placeholder-transparent appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-200" />
           </div>
-          <div class="w-full md:w-1/2 px-3">
+          <div class="w-full md:w-1/2 md:mb-0 px-3">
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               Last Name
             </label>
@@ -96,12 +96,12 @@ export default function SignUp(props) {
               name="floating_last_name"
               id="lastName"
               placeholder="smith"
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-200" />
+              className="focus:placeholder-transparent appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-200" />
           </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            <label class="focus:placeholder-transparent block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               Password
             </label>
             <input
@@ -110,7 +110,7 @@ export default function SignUp(props) {
               type="password"
               name="floating_password"
               id="password"
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-200" placeholder="********" />
+              className="focus:placeholder-transparent appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-200" placeholder="********" />
           </div>
           <div class="w-full px-3">
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
@@ -122,7 +122,7 @@ export default function SignUp(props) {
               type="password"
               name="floating_password"
               id="confirmPassword"
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-200" placeholder="********" />
+              className="focus:placeholder-transparent appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-200" placeholder="********" />
             <p className="text-red-500 text-xs italic">{validationMessage}</p>
           </div>
         </div>
@@ -137,30 +137,12 @@ export default function SignUp(props) {
               type="text"
               id="email"
               name="floating_email"
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque" />
-          </div>
-          <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-              State
-            </label>
-            <div class="relative">
-              <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                <option>New Mexico</option>
-                <option>Missouri</option>
-                <option>Texas</option>
-              </select>
-              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-              </div>
-            </div>
-          </div>
-          <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-              Zip
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210" />
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="erickinder@gmail.com" />
           </div>
         </div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline">
+          Sign Up
+        </button>
       </form>
     </div>
   )
