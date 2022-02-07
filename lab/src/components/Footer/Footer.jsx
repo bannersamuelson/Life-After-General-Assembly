2331b4188fd791e5d848e0f7ac3c8f61cefcd141
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-02-07 13:44:51 -0500

    update footer

--- lab/src/components/Footer/Footer.jsx
+++ lab/src/components/Footer/Footer.jsx
@@ -3,23 +3,14 @@ import logo from '../../images/database.jpg'
 
 
 const Footer = () =>
-  // <footer>
-  //   <h1> © 2022 L.A.G.A. All Rights Reserved</h1>
-  //   <div>
-  //     <img className="mr-3 h-10" viewBox="0 0 52 72" src={img} alt="LinkedIn logo" />
-  //     <a href="https://www.linkedin.com/in/rod-raymond-281a0284/" style={{ textDecoration: "none" }}>Rod Raymond</a>
-  //     <a href="https://www.linkedin.com/in/galinakishirova/" style={{ textDecoration: "none" }}>Galina Kishirova</a>
-  //     <a href="https://www.linkedin.com/in/michaelabebe5/" style={{ textDecoration: "none" }}>Michael Abebe</a>
-  //     <a href="https://www.linkedin.com/in/banner-samuelson/" style={{ textDecoration: "none" }}>Banner Samuelson</a>
-  //   </div>
-
-  //   </footer>
 
   <footer class="p-4 bg-white sm:p-6 dark:bg-gray-800">
     <div class="md:flex md:justify-between">
-        <div class="mb-6 md:mb-0 flex items-center">
+        <div class="mb-6 md:mb-0">
+            <a href="#" target="_blank" class="flex items-center">
                 <img src={logo} class="mr-4 h-10" alt="FlowBite Logo" />
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">L.A.G.A</span>
+            </a>
         </div>
         <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
