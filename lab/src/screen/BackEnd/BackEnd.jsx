fe15c4ab9b9ffce404154bee680900fda5be92fc
Banner Samuelson <bannersamuelson@gmail.com>
2022-02-07 17:01:44 -0500

    backend

--- lab/src/screen/BackEnd/BackEnd.jsx
+++ lab/src/screen/BackEnd/BackEnd.jsx
@@ -79,6 +79,10 @@ export default function Test(props) {
           <div class="flex flex-col justify-between p-4 leading-normal">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Back-End Resources</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">In technology development, full stack refers to an entire computer system or application from the front end to the back end and the code that connects the two.<span className="text-gray-100"> The back end of a computer system encompasses “behind-the-scenes” technologies such as the database and operating system.</span></p>
+            <Link to='/addcourse' className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Add Course
+
+            </Link>
           </div>
         </div>
       </div>
