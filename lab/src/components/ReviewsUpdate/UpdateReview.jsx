7da62e557bef11a3dd852146660dbd1675ad788e
Banner Samuelson <bannersamuelson@gmail.com>
2022-02-08 14:44:11 -0500

    updated

--- lab/src/components/ReviewsUpdate/UpdateReview.jsx
+++ lab/src/components/ReviewsUpdate/UpdateReview.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function UpdateReview(props) {
+  const [toggle, setToggle] = useState(false);
   console.log(props);
   const navigation = useNavigate();
   const [newReview, setNewReview] = useState({
@@ -27,6 +28,8 @@ export default function UpdateReview(props) {
     console.log(newReview.author);
     let res = await updateReview(newReview, props._id, props.review._id);
     console.log(res);
+    setToggle((prevToggle) => !prevToggle);
+
     navigation("/");
   };
 
@@ -37,34 +40,58 @@ export default function UpdateReview(props) {
       [id]: value,
     }));
   };
-  return <div>
-    <form onSubmit={handleSubmit}
-      className="mr-auto ml-auto w-full max-w-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <label >Review</label>
-      <br />
-      <input
-        type="text"
-        placeholder="write your review"
-        id="review"
-        value={newReview.review}
-        onChange={handleInput}
-        className="flex items-center border-b border-teal-500 py-2 text-teal-700"
-      />
-      <br />
-      <label >Rate</label>
-      <br />
-      <input
-        type="number"
-        placeholder="image link"
-        id="rate"
-        value={newReview.rate}
-        onChange={handleInput}
-        className="flex items-center border-b border-teal-500 py-2 text-teal-700"
-      />
-      <br />
-      <button className="flex-shrink-0 bg-blue-900 hover:bg-blue-900 border-blue-700 hover:border-blue-900 text-sm border-4 text-white py-1 px-2 rounded text-xl">
-        Update review
-      </button>
-    </form>
-  </div>;
+
+  const handleNumberInput = (e) => {
+    const { id, value } = e.target;
+    console.log(value);
+    if (value > 5 || value < 1) {
+      alert("rating can't be bigger than 5");
+      return null;
+    }
+    setNewReview((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+
+
+  return (
+    <div className="w-full mr-auto ml-auto mt-auto mb-auto max-w-xs text-xl">
+      <h2 className="m-5 text-center text-slate-600 font-bold ">Update Review</h2>
+      <form onSubmit={handleSubmit}
+        className="mr-auto ml-auto w-full max-w-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <label className="text-slate-500">Review</label>
+        <br />
+        <input
+          className=" placeholder:text-slate-300 grid focus:placeholder-transparent bg-slate-100 rounded border shadow border-slate-200 leading-normal resize-none w-full h-14 px-3 font-medium text-slate-800 focus:outline-none focus:bg-white"
+          type="text"
+          id="review"
+          value={newReview.review}
+          onChange={handleInput}
+          placeholder='Type your review'
+          required />
+        <br />
+        <label className="text-slate-500">Rate</label>
+        <br />
+        <input
+          type="number"
+          placeholder="1 - 5"
+          id="rate"
+          value={newReview.rate}
+          onChange={handleNumberInput}
+          required
+          className="focus:border-slate-300 text-slate-800 pl-2 flex items-center border shadow border-slate-200 py-2 rounded focus:border-collapse bg-slate-100"
+        />
+        <br />
+        <button className="flex-shrink-0 bg-sky-400 hover:bg-sky-600 hover:border-tskyeal-700 text-sm text-white py-1 px-2 rounded">
+          Update
+        </button> {toggle && <button onClick={(e) => { e.preventDefault(); setToggle((prevToggle) => !prevToggle); }}
+          className="flex-shrink-0 text-sm text-slate-400 hover:text-slate-500 py-1 px-2 rounded">
+          Close
+        </button>}
+
+      </form>
+    </div>
+  );
 }
