7ffad82f6f5124f324bfd9362c3aca7201dcfdb6
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-02-07 19:44:16 -0500

    show and hide password entry field

--- lab/src/screen/Login/Login.jsx
+++ lab/src/screen/Login/Login.jsx
@@ -6,6 +6,8 @@ import { loginUser } from "../../services/apiconfig";
 
 export default function Login(props) {
   const navigate = useNavigate()
+  const [hidePassword, setHidePassword] = useState("password");
+  const [oldPassword, setOldPassword] = useState("text");
   const [user, setUser] = useState({
     email: "",
     password: "",
@@ -41,7 +43,12 @@ const handleSubmit = async (e)=>{
       [id]: value,
     }))
   };
-
+  const Toggle = (e) => {
+    e.preventDefault();
+    let temp = hidePassword;
+    setHidePassword(oldPassword);
+    setOldPassword(temp);
+}
   return (
     <div className="grid justify-items-center w-full mt-10">
       <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -62,12 +69,13 @@ const handleSubmit = async (e)=>{
             Password
           </label>
           <input
-            type="text"
+            type={hidePassword}
             placeholder="********"
             id="password"
             value={user.password}
             onChange={handleInput}
             className="focus:placeholder-transparent shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+          <button onClick={(e)=>{Toggle(e)}}>Show Password</button>
           <p className="text-slate-500 text-xs italic">Not a user? <Link className="underline hover:cursor-pointer hover:text-red-100" to='/signup'>Sign up!</Link></p>
         </div>
         <div className="flex items-center justify-between">
