c23c3536408227700df3a47f5192e54d396abfe0
Banner Samuelson <bannersamuelson@gmail.com>
2022-02-07 20:21:36 -0500

    fixed browse courses

--- lab/src/screen/Courses/Courses.jsx
+++ lab/src/screen/Courses/Courses.jsx
@@ -10,15 +10,16 @@ import backend from '../../images/backend.png'
 export default function Courses() {
   return <div>
     <div className="w-full bg-gradient-to-r from-blue-800 to-blue-900 pb-10">
-      <div className="grid grid-cols-1 justify-items-center">
-        <h1 className="flex flex-col justify-center items-center pt-14 text-8xl text-extrabold text-extrabold text-slate-200">Browse Courses</h1>
-      </div>
+
+      <h1 className="flex flex-col justify-center items-center pt-14 
+      text-5xl md:text-8xl text-extrabold text-extrabold text-slate-200">Browse Courses</h1>
+
       <section className=" mx-auto px-4 sm:px-6 lg:px-4 py-12">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 hover:text-slate-100 lg:px-0 gap-4">
           <Link to='/Front-End' className="group w-full transition ease-in-out delay-150 hover:cursor-pointer   hover:-translate-y-1 hover:scale-105 duration-300 
           hover:bg-gradient-to-r from-teal-200 to-teal-500  bg-slate-100 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
             <div className="mb-8">
-              <img className="object-center object-cover rounded-full h-36 w-36" src={frontImg} alt="photo" />
+              <img className="object-center object-cover rounded-full h-36 w-36" src={frontImg} alt="frontend" />
             </div>
             <div className="text-center">
               <h1 className="text-2xl text-slate-800 font-bold mb-2">FRONT-END</h1>
@@ -28,7 +29,7 @@ export default function Courses() {
           <Link to='/Back-End' className="group w-full transition ease-in-out delay-150 hover:cursor-pointer   hover:-translate-y-1 hover:scale-105 duration-300 
           hover:bg-gradient-to-r from-red-200 to-red-500  bg-slate-100 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
             <div className="mb-8">
-              <img className="object-center object-cover rounded-full h-36 w-36" src={backend} alt="photo" />
+              <img className="object-center object-cover rounded-full h-36 w-36" src={backend} alt="backend" />
             </div>
             <div className="text-center">
               <h1 className="text-2xl text-slate-800 font-bold mb-2">BACK-END</h1>
@@ -38,7 +39,7 @@ export default function Courses() {
           <Link to='/database' className="group w-full transition ease-in-out delay-150 hover:cursor-pointer  hover:text-slate-100 hover:-translate-y-1 hover:scale-105 duration-300
            hover:bg-gradient-to-r from-yellow-200 to-yellow-400 bg-slate-100 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
             <div className="mb-8">
-              <img className="object-center object-cover rounded-full h-36 w-36" src={dataImg} alt="photo" />
+              <img className="object-center object-cover rounded-full h-36 w-36" src={dataImg} alt="data" />
             </div>
             <div className="text-center">
               <h1 className="text-2xl text-slate-800 font-bold mb-2">DATABASE</h1>
@@ -48,7 +49,7 @@ export default function Courses() {
           <Link to='/devops' className="group w-full transition ease-in-out delay-150 hover:cursor-pointer  hover:text-slate-100 hover:-translate-y-1 hover:scale-105 duration-300
            bg-slate-100 hover:bg-gradient-to-r from-orange-400 to-orange-600 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
             <div className="mb-8">
-              <img className="object-center object-cover rounded-full h-36 w-36" src={devops} alt="photo" />
+              <img className="object-center object-cover rounded-full h-36 w-36" src={devops} alt="devops" />
             </div>
             <div className="text-center">
               <h1 className="text-2xl text-slate-800 font-bold mb-2">DEVOPS</h1>
@@ -58,7 +59,7 @@ export default function Courses() {
           <Link to='/mobile' className="group w-full transition ease-in-out delay-150 hover:cursor-pointer  hover:text-slate-100 hover:-translate-y-1 hover:scale-105 duration-300
            hover:bg-gradient-to-r from-blue-400 to-blue-600 bg-slate-100 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
             <div className="mb-8">
-              <img className="object-center object-cover rounded-full h-36 w-36" src={phoneImg} alt="photo" />
+              <img className="object-center object-cover rounded-full h-36 w-36" src={phoneImg} alt="phone" />
             </div>
             <div className="text-center">
               <h1 className="text-2xl text-slate-800 font-bold mb-2">MOBILE</h1>
