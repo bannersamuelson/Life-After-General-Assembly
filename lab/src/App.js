2270631ff51e39aba699367265df23e8ab483f92
Banner Samuelson <bannersamuelson@gmail.com>
2022-02-09 11:33:33 -0500

    fixed

--- lab/src/App.js
+++ lab/src/App.js
@@ -31,42 +31,83 @@ function App() {
       <Layout user={user} setUser={setUser} {...loginUser} setLoginUser={setLoginUser}>
         <Routes>
           <Route path="/" element={<>
-            <HomePage user={user} setUser={setUser} />
+            <HomePage
+              user={user}
+              setUser={setUser} />
             <Test user={user} setUser={setUser} home={true} />
           </>} />
           <Route path="/courses" element={<>
 
-            <Courses user={user} setUser={setUser} home={true} />
+            <Courses user={user} setUser={setUser} />
+            <Test user={user} setUser={setUser}
           </>} />
           <Route path="/:id" element={
             <>
-              <FetchCourses user={user} setUser={setUser} setLoginUser={setLoginUser} {...loginUser} review={review} setReview={setReview}></FetchCourses>
+              <FetchCourses
+                user={user}
+                setUser={setUser}
+                setLoginUser={setLoginUser}
+                {...loginUser}
+                review={review}
+                setReview={setReview}>
+              </FetchCourses>
               {/* <Test user={user} setUser={setUser} home={true} /> */}
             </>} />
           <Route path="/Front-End" element={
             <>
-              <FrontEnd user={user} setUser={setUser} loginUser={loginUser} setLoginUser={setLoginUser} types={"Front-End"} />
+              <FrontEnd
+                user={user}
+                setUser={setUser}
+                loginUser={loginUser}
+                setLoginUser={setLoginUser}
+                types={"Front-End"} />
             </>} />
 
-          <Route path="/Back-End" element={<>
-            <BackEnd user={user} setUser={setUser} loginUser={loginUser} setLoginUser={setLoginUser} types={"Back-End"}>
-            </BackEnd>
-          </>} />
+          <Route path="/Back-End" element={
+            <>
+              <BackEnd
+                user={user}
+                setUser={setUser}
+                loginUser={loginUser}
+                setLoginUser={setLoginUser}
+                types={"Back-End"}>
+              </BackEnd>
+            </>} />
           <Route path="/portfolio" element={<>
-            <Portfolio user={user} setUser={setUser} loginUser={loginUser} setLoginUser={setLoginUser} types={"Portfolio"}>
+            <Portfolio
+              user={user}
+              setUser={setUser}
+              loginUser={loginUser}
+              setLoginUser={setLoginUser}
+              types={"Portfolio"}>
             </Portfolio>
           </>} />
           <Route path="/database" element={<>
-            <Database user={user} setUser={setUser} loginUser={loginUser} setLoginUser={setLoginUser} types={"Database"}>
+            <Database
+              user={user}
+              setUser={setUser}
+              loginUser={loginUser}
+              setLoginUser={setLoginUser}
+              types={"Database"}>
             </Database>
           </>} />
+
           <Route path="/devops" element={<>
-            <Devops user={user} setUser={setUser} loginUser={loginUser} setLoginUser={setLoginUser} types={"Dev-Ops"}>
+            <Devops
+              user={user}
+              setUser={setUser}
+              loginUser={loginUser}
+              setLoginUser={setLoginUser}
+              types={"Dev-Ops"}>
             </Devops>
           </>} />
           <Route path="/mobile" element={<>
-
-            <Mobile user={user} setUser={setUser} loginUser={loginUser} setLoginUser={setLoginUser} types={"Mobile"}>
+            <Mobile
+              user={user}
+              setUser={setUser}
+              loginUser={loginUser}
+              setLoginUser={setLoginUser}
+              types={"Mobile"}>
             </Mobile>
           </>} />
           <Route
@@ -76,16 +117,30 @@ function App() {
                 user={user} setUser={setUser} />
             </>} />
           <Route path="/login" element={<>
-            <Login loginUser={loginUser} setLoginUser={setLoginUser} />
+            <Login
+              loginUser={loginUser}
+              setLoginUser={setLoginUser} />
           </>} />
           <Route path="/addcourse" element={<>
-            <AddCourse user={user} setUser={setUser} {...loginUser} />
+            <AddCourse u
+              ser={user}
+              setUser={setUser}
+              {...loginUser} />
           </>} />
           <Route path="/courses/addreview/:id" element={<>
-            <AddReview user={user} setUser={setUser} review={review} setReview={setReview} />
+            <AddReview
+              user={user}
+              setUser={setUser}
+              review={review}
+              setReview={setReview} />
           </>} />
           <Route path="update/review" element={
-            <UpdateReview user={user} setUser={setUser} review={review} setReview={setReview} {...loginUser} />
+            <UpdateReview
+              user={user}
+              setUser={setUser}
+              review={review}
+              setReview={setReview}
+              {...loginUser} />
           } />
         </Routes>
       </Layout>
