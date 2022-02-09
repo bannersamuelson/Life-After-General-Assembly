c7e4d0906f6de4eeb8310fdc3ee5c9a5f8381fb4
Banner Samuelson <bannersamuelson@gmail.com>
2022-02-09 11:26:52 -0500

    nav

--- lab/src/components/Nav/Nav.jsx
+++ lab/src/components/Nav/Nav.jsx
@@ -60,7 +60,7 @@ const Nav = (props) => {
         <div className="flex justify-between">
           <NavLink to="/" className="flex active">
             <img className="mr-3 h-10" viewBox="0 0 52 72" alt="logo" src={img} />
-            <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">L.A.G.A</span>
+            <span className="self-center text-lg font-bold whitespace-nowrap dark:text-white">Life After General Assembly</span>
           </NavLink>
           <div className="hidden w-full md:flex md:justify-between items-center md:content-end md:w-auto md:order-1">
             <ul className="text-2xl flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium">
